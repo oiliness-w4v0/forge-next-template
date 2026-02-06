46cb349b545bc188cb809e2810d5e91ae42d2c35
czq040902 <16615812538@163.com>
2026-02-07 00:55:24 +0800

    eslint

--- build.ts
+++ build.ts
@@ -1,9 +1,9 @@
 import Bun from 'bun'
 
 Bun.build({
-    entrypoints: ["./src/main.ts", "./src/preload.ts"],
-    outdir: './.vite/build',
-    sourcemap: "external",
-    external: ['electron'],
-    target: 'node'
+  entrypoints: ['./src/main.ts', './src/preload.ts'],
+  outdir: './.vite/build',
+  sourcemap: 'external',
+  external: ['electron'],
+  target: 'node',
 })
--- forge.config.ts
+++ forge.config.ts
@@ -1,25 +1,25 @@
 import type { ForgeConfig } from '@electron-forge/shared-types'
-import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
+import { MakerDeb } from '@electron-forge/maker-deb'
+import { MakerZIP } from '@electron-forge/maker-zip'
 
 const config: ForgeConfig = {
-    packagerConfig: {
-        asar: true,
-        ignore: [
-            /^\/src/,
-            /^\/index\.html$/,
-            /^\/vite\.config\.ts$/,
-            /^\/build\.ts$/,
-            /^\/node_modules/,
-            /^\/.git/,
-            /^\/out/,
-            // 不要忽略 .vite 目录（这是构建输出）
-        ],
-    },
-    makers: [
-        new MakerZIP(), // 所有平台都生成 ZIP
-        new MakerDeb({}),
+  packagerConfig: {
+    asar: true,
+    ignore: [
+      /^\/src/,
+      /^\/index\.html$/,
+      /^\/vite\.config\.ts$/,
+      /^\/build\.ts$/,
+      /^\/node_modules/,
+      /^\/.git/,
+      /^\/out/,
+      // 不要忽略 .vite 目录（这是构建输出）
     ],
+  },
+  makers: [
+    new MakerZIP(), // 所有平台都生成 ZIP
+    new MakerDeb({}),
+  ],
 }
 
 export default config
